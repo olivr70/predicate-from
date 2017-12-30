1bade4b342dde92737d8efccb103fb5271889a0e
olivr70 <o.chevet@gmail.com>
2017-12-30 08:04:51 +0100

    Upgraded build dependancies (rollup...)

--- test/main_spec.js
+++ test/main_spec.js
@@ -8,23 +8,23 @@ let predicateFrom = require("../lib/main.cjs.js")
 describe('predicateFrom', function () {
   let isEven = function (x) { return x % 2 === 0 }
   it("should leave a function untouched", () => {
-    expect(predicateFrom(isEven)).to.be.a.function;
+    expect(predicateFrom(isEven)).to.be.a('function');
     expect(predicateFrom(isEven)).to.equal(isEven);
     expect(predicateFrom(isEven)(4)).to.be.true;
     expect(predicateFrom(isEven)(3)).to.be.false;
   });
   it("should convert a string to a predicate", () => {
-    expect(predicateFrom('a')).to.be.a.function;
+    expect(predicateFrom('a')).to.be.a('function');
     expect(predicateFrom('a')("a")).to.be.true;
     expect(predicateFrom('a')("aa")).to.be.false;
   });
   it("should convert a number to a predicate", () => {
-    expect(predicateFrom(1)).to.be.a.function;
+    expect(predicateFrom(1)).to.be.a('function');
     expect(predicateFrom(1)(1)).to.be.true;
     expect(predicateFrom(1)(2)).to.be.false;
   });
   it("should convert a boolean to a predicate", () => {
-    expect(predicateFrom(true)).to.be.a.function;
+    expect(predicateFrom(true)).to.be.a('function');
     expect(predicateFrom(true)(true)).to.be.true;
     expect(predicateFrom(true)(false)).to.be.false;
     expect(predicateFrom(false)(false)).to.be.true;
@@ -33,7 +33,7 @@ describe('predicateFrom', function () {
   it("should convert a regular expression to a predicate", () => {
     let digits = /[0-9]+/
     let onlyDigits = /^[0-9]+$/
-    expect(predicateFrom(digits)).to.be.a.function;
+    expect(predicateFrom(digits)).to.be.a('function');
     expect(predicateFrom(digits)('0')).to.be.true;
     expect(predicateFrom(digits)('01232')).to.be.true;
     expect(predicateFrom(digits)('aa 01232 bb')).to.be.true;
@@ -65,7 +65,7 @@ describe('predicateFrom', function () {
     expect(predicateFrom(['a', 'b'])('c')).to.be.false;
   });
   it("should convert a plain JS object with native properties to a predicate", () => {
-    expect(predicateFrom({ a: 1 })).to.be.a.function;
+    expect(predicateFrom({ a: 1 })).to.be.a('function');
     expect(predicateFrom({ a: 1 })({ a: 1 })).to.be.true;
     expect(predicateFrom({ a: 1 })({ a: 1, b: 2 })).to.be.true;
     expect(predicateFrom({ a: 1 })({ a: 2 })).to.eq(false);
